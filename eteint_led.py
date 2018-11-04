340a207968fe1cc44ea5cf5aab03d0cdcedd9aaf
Pedro <ptrouillard@gmail.com>
2018-11-04 12:48:54 +0000

    Scripts simples d'activation et de désactivation des led

---
+++ eteint_led.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+
+broche = int(sys.argv[1])
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(broche, GPIO.OUT)
+
+GPIO.output(broche, GPIO.LOW)
