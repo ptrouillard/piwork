fe35bd04c4a5830f0d34158e78020debc36b850c
Pierre <ptrouillard@gmail.com>
2020-03-23 15:59:20 +0100

    Update script therm_toffile.py to get data from two sources

--- scripts/therm_tofile.py
+++ scripts/therm_tofile.py
@@ -14,15 +14,18 @@ def recupTemp (contenuFich) :
     temperature = temperature / 1000
     return temperature
 
-def sauvegarde (temperature, date, emplacement) :
+def sauvegarde (date, temperature1, temperature2, emplacement) :
     fichierSauvegarde = open(emplacement, "a")
-    fichierSauvegarde.write(str(date)+"   ")
-    fichierSauvegarde.write(str(temperature)+'\r\n')
+    fichierSauvegarde.write(str(date)+";")
+    fichierSauvegarde.write(str(temperature1)+";")
+    fichierSauvegarde.write(str(temperature2)+"\r\n");
     fichierSauvegarde.close()
     
 while True :
     date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    contenuFich = lireFichier("/sys/bus/w1/devices/28-021091776496/w1_slave")
-    temperature = recupTemp(contenuFich)
-    sauvegarde(temperature, date, "Temperature.txt")
+    contenuFich2 = lireFichier("/sys/bus/w1/devices/28-021091776496/w1_slave")
+    contenuFich1 = lireFichier("/sys/bus/w1/devices/28-020f917745e2/w1_slave")
+    temperature2 = recupTemp(contenuFich2)
+    temperature1 = recupTemp(contenuFich1)
+    sauvegarde(date, temperature1, temperature2,"Temperature.csv")
     time.sleep(60)
